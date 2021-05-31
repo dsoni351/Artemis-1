c2571d64a9a587d41e64fe4e10196ff7dd0a38b4
Dheeraj <dheerajsoni351@gmail.com>
2021-05-31 20:31:37 +0530

    commit changes

--- src/app/shared/Sidebar.js
+++ src/app/shared/Sidebar.js
@@ -53,7 +53,7 @@ class Sidebar extends Component {
     return (
       <nav className="sidebar sidebar-offcanvas" id="sidebar">
         <div className="text-center sidebar-brand-wrapper d-flex align-items-center" style={{backgroundColor: "#f3f3f3", borderRadius: "42px"}}>
-          <a className="sidebar-brand brand-logo" href="/index.html"><img src="https://shuffle.dev/artemis-assets/logos/artemis-logo-light.svg" alt="logo" /></a>
+          <p className="sidebar-brand brand-logo"style={{cursor: "pointer"}}><img src="https://shuffle.dev/artemis-assets/logos/artemis-logo-light.svg" alt="logo" /></p>
           
         </div>
         <h6 className="" style={{color: "white",marginLeft: "15px", marginTop: "34px"}}>Main</h6>
